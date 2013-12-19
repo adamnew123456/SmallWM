c984b6fa62d98036699db42c377d6dcd23ae182a
adamnew123456 <adamnew123456@gmail.com>
2013-12-19 17:34:46 -0500

    Added some of the basic X events.
    This includes all the basic event types - the keyboard subhandlers are
    still unwritten.

--- rewrite/event.c
+++ rewrite/event.c
@@ -7,6 +7,22 @@ events_t *event_init(smallwm_t *smallwm)
     events_t *events = malloc(sizeof(events_t));
     events->event_callbacks = new_table();
     events->key_callbacks = new_table();
+
+    int idx = 0;
+    while (event_callbacks[idx])
+    {
+        add_table(events->event_callbacks, event_types[idx], event_callbacks[idx]);
+        idx++;
+    }
+
+    idx = 0;
+    while (key_callbacks[idx])
+    {
+        add_table(events->key_callbacks, keysym_types[idx], key_callbacks[idx]);
+        idx++;
+    }
+
+    events->wm = smallwm;
     return events;
 }
 
@@ -20,4 +36,131 @@ void add_keyboard_handler_event(events_t *events, KeySym key, event_callback_t *
 void add_handler_event(events_t *events, int event, event_callback_t *callback)
 {
     add_table(events->event_callbacks, event, callback);
+}
+
+// Dispatches on X events
+void run_loop_event(events_t *events)
+{
+    XEvent event;
+    while (1)
+    {
+        XNextEvent(events->wm->display, *event);
+
+        event_callback_t *handler = get_table(events->event_callbacks, event.type);
+        if (handler)
+            handler(events->wm, &event);
+    }
+}
+
+// Handle keyboard commands by simple table dispatch
+void on_keypress_event(smallwm_t *wm, XEvent *event)
+{
+    int nkeys;
+    KeySym *keysym = NULL;
+    keysym = XGetKeyboardMapping(wm->display, event->xkey->keycode, 1, &nkeys);
+
+    event_callback_t *handler = get_table(events->key_callbacks, *keysym);
+    if (handler)
+        handler(wm, event);
+}
+
+/* Handle a button press, which can do one of a few things:
+ *  (a) Changes the focus when clicking on an unfocused window
+ *      (in this case, the mask will not be equal to MASK)
+ *  (b) This function starts moving or resizing
+ *  (c) This function shows a previously hidden window
+ *  (d) Launches a shell
+*/
+void on_buttonpress_event(smallwm_t *wm, XEvent *event)
+{
+    client_t *client = get_table(wm->clients, event->subwindow);
+    icon_t *icon = get_table(wm->icons, event->window);
+
+    //  click on the root window
+    if (!(client || icon) && ev.xbutton.button == LAUNCH)
+    {
+        shell_launch_wm(wm);
+    }
+    else if (icon)
+    {
+        to_client(icon);
+    }
+    else if (client)
+    {
+        if (event->xbutton->state == MASK)
+        {
+            switch (event->xbutton.button)
+            {
+            case MOVE:
+            {
+                wm->movement.state = MR_MOVE;
+                wm->movement.event = event->xbutton;
+                wm->movement.window = wm->window;
+                begin_moveresize_cleint(client);
+            }; break;
+            case RESZ:
+            {
+                wm->movement.state = MR_RESZ;
+                wm->movement.event = event->xbutton;
+                wm->movement.window = event->window;
+                begin_moveresize_client(client);
+            }; break;
+            }
+        }
+        else
+        {
+            client = get_table(wm->clients, event->window);
+            refocus_wm(wm, client->window);
+        }
+    }
+}
+
+// Stops moving/resizing if there is anything which is being moved/resized
+void on_buttonrelease_event(smallwm_t *wm, XEvent *event)
+{
+    if (wm->movement.state != MV_NONE)
+    {
+        client_t *client = get_table(wm->clients, event->xbutton.window);
+        end_moveresize_client(client);
+
+        wm->movement.state = MV_NONE;
+    }
+}
+
+// Updates the window which is being moved or resized
+void on_motionnotify_event(smallwm_t *wm, XEvent *event)
+{
+    if (wm->movement.state == MV_NONE)
+        return;
+
+    // Go ahead and get the final movement to avoid needless updating
+    while (XCheckTypedEvent(wm->display, MotionNotify, event));
+
+    // Do the movement
+    int xdiff, ydiff;
+    xdiff = ev->xbutton.x_root - wm->movement.event.x_root;
+    ydiff = ev->xbutton.y_root - wm->movement.event.y_root;
+
+    // Get the old location of the window
+    XWindowAttributes attr;
+    XGetWindowAttributes(wm->display, wm->movement.client->mvresz_placeholder, &attr);
+
+    if (wm->movement.state == MR_MOVE)
+        XMoveWindow(wm->display, wm->movement.client->mvresz_placeholder,
+                attr.x + xdiff, attr.y + ydiff);
+
+    if (wm->movement.state == MR_RESZ)
+    {
+        int new_width = attr.w + xdiff;
+        int new_height = attr.h + ydiff;
+
+        XResizeWindow(wm->display, wm->movement.client->mvresz_placeholder,
+                (new_width > 1 ? new_width : 1), (new_height > 1 ? new_height : 1));
+    }
+}
+
+// Initialize a new client
+void on_mapnotify_event(smallwm_t *wm, XEvent *event)
+{
+    add_client_wm(wm, event->xmap.window);
 }
--- rewrite/event.h
+++ rewrite/event.h
@@ -1,9 +1,12 @@
 #ifndef __SMALLWM_EVENT__
 #define __SMALLWM_EVENT__
 
+#include <signal.h>
 #include <stdlib.h>
 
+#include "client.h"
 #include "struct.h"
+#include "wm.h"
 #include "x11.h"
 
 // A generic event callback
@@ -13,4 +16,40 @@ events_t *event_init(smallwm_t *smallwm);
 void add_keyboard_handler_event(events_t *events, KeySym key, event_callback_t *callback);
 void add_handler_event(events_t *events, KeySym key, event_callback_t *callback);
 void run_loop_event(events_t *events);
+
+void on_keypress_event(smallwm_t *wm, XEvent *event);
+void on_buttonpress_event(smallwm_t *wm, XEvent *event);
+void on_buttonrelease_event(smallwm_t *wm, XEvent *event);
+void on_motionnotify_event(smallwm_t *wm, XEvent *event);
+void on_mapnotify_event(smallwm_t *wm, XEvent *event);
+
+int event_types[] = { KeyPress, ButtonPress, ButtonRelease, MotionNotify, MapNotify };
+event_callback_t event_callbacks[] = { on_keypress_event, on_buttonpress_event,
+                                       on_buttonrelease_event, on_motionnotify_event,
+                                       on_mapnotify_event , NULL };
+
+void on_pageup_event(smallwm_t *wm, XEvent *event);
+void on_pagedown_event(smallwm_t *wm, XEvent *event);
+void on_m_event(smallwm_t *wm, XEvent *event);
+void on_c_event(smallwm_t *wm, XEvent *event);
+void on_x_event(smallwm_t *wm, XEvent *event);
+void on_h_event(smallwm_t *wm, XEvent *event);
+void on_r_event(smallwm_t *wm, XEvent *event);
+void on_bracketright_event(smallwm_t *wm, XEvent *event);
+void on_bracketleft_event(smallwm_t *wm, XEvent *event);
+void on_backslash_event(smallwm_t *wm, XEvent *event);
+void on_left_event(smallwm_t *wm, XEvent *event);
+void on_right_event(smallwm_t *wm, XEvent *event);
+void on_up_event(smallwm_t *wm, XEvent *event);
+void on_down_event(smallwm_t *wm, XEvent *event);
+
+int keysym_types[] = { XK_Page_Up, XK_Page_Down, XK_m, XK_c, XK_x, XK_h, XK_r,
+                        XK_bracketright, XK_bracketleft, XK_backslash, XK_Left,
+                        XK_Right, XK_Up, XK_Down , XK_Escape, XK_comma, XK_period };
+event_callback_t key_callbacks[] = { on_pageup_event, on_pagedown_event,
+                                     on_m_event, on_c_event, on_x_event, on_h_event,
+                                     on_r_event, on_bracketright_event, on_bracketleft_event,
+                                     on_backslash_event, on_left_event, on_right_event,
+                                     on_up_event, on_down_event, on_escape_event, on_comma_event,
+                                     on_period_event, NULL};
 #endif
