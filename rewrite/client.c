6caf348a1b5b672dc6e63002f84865918eee422f
adamnew123456 <adamnew123456@gmail.com>
2013-12-19 10:32:34 -0500

    Added the moving and resizing code in the client.

--- rewrite/client.c
+++ rewrite/client.c
@@ -33,6 +33,58 @@ void request_close_client(client_t *client)
     XSendEvent(client->wm->display, client->window, False, NoEventMask, &close_event);
 }
 
+// Begins moving or resizing a client
+void begin_moveresize_client(client_t *client)
+{
+    if (client->state != C_VISIBLE)
+        return;
+    client->state = C_MOVERESZ;
+    
+    // Update the location and size attributes before 
+    XWindowAttributes attr;
+    XGetWindowAttributes(client->wm->display, client->window, &attr);
+
+    // Remove the window and create a placeholder
+    XUnmapWindow(client->wm->display, client->window);
+    client->mvresz_placeholder = XCreateSimpleWindow(client->wm->display,
+            client->wm->root, attr.x, attr.y, attr.width, attr.height, 1, 
+            BlackPixel(client->wm->display, client->wm->screen), 
+            WhitePixel(client->wm->display, client->wm->screen));
+
+    // Ask the WM module to ignore this window
+    XSetWindowAttributes attr;
+    attr.override_redirect;
+    XChangeWindowAttributes(client->wm->display, client->mvresz_placeholder, CWOverrideRedirect, &attr);
+
+    // Put up the window and force the pointer into the window
+    XMapWindow(client->wm->display, client->mvresz_placeholder);
+    XGrabPointer(client->wm->display, client->mvresz_placeholder, True,
+            PointerMotionMask | ButtonReleaseMask,
+            GrabModeAsync, GrabModeAsync, Node, Node, CurrentTime);
+    XRaiseWindow(client->wm->display, client->mvresz_placeholder);
+}
+
+// Stop moving or resizing the client
+void end_moveresz_client(client_t *client)
+{
+    if (client->state != C_MOVERESZ)
+        return;
+    client->state = C_VISIBLE;
+
+    // Unsubscribe from pointer events and free the pointer
+    XUngrabPointer(client->wm->display, CurrentTime);
+
+    XWindowAttributes attr;
+    XGetWindowAttribues(client->wm->display, client->mvresz_placeholder);
+    XDestroyWindow(clietn->wm->display, client->mvresz_placeholder);
+
+    // Place the original window
+    XMapWindow(client->wm->display, client->window);
+    XMoveResizeWindow(client->wm->display, client->window, attr.x, attr.y, attr.width, attr.height);
+
+    raise_client(client);
+}
+
 // Removes a client once the window closes
 void destroy_client(client_t *client)
 {
