f3f2103fe689af17703309f3f0a8395c65a0e511
adamnew123456 <adamnew123456@gmail.com>
2013-12-20 18:40:03 -0500

    Added an X error handler to prevent SmallWM from crashing on X errors.

--- rewrite/smallwm.c
+++ rewrite/smallwm.c
@@ -3,8 +3,21 @@
 #include "wm.h"
 #include "x11.h"
 
+// Prints out X errors, but doesn't crash the program like the normal handler
+int x_error_handler(Display *display, XErrorEvent *event)
+{
+    char err_desc[500];
+    XGetErrorText(display, event->error_code, err_desc, 500);
+
+    printf("X Error: { display = %s, serial = %lu, error = '%s', request = %d, minor = %d }\n",
+            XDisplayName(NULL), event->serial, err_desc, event->request_code, event->minor_code);
+}
+
 int main()
 {
+    // Don't crash when an error is generated
+    XSetErrorHandler(x_error_handler);
+
     smallwm_t *wm = init_wm();
     events_t *events = event_init(wm);
 
